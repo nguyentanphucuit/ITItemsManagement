5f0fa5a725f26cbd9717d965047bb055e3cd4c8b
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2020-06-27 18:18:12 +0700

    charge file

---
+++ Web/js/affix.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".edulist").css({
+      "text-align": "center",
+      "font-size": "20px",
+      "font-family": "josefin sans",
+      "font-weight": "600"
+    });
+    $(".rightcred").css("text-align", "right");
+    $(".credlist").addClass("well").css("background-image", "url('https://lh3.googleusercontent.com/if7yyyMZa92Uzo3hdTFx2Mel9nUqQ9uMsQphSYvz0s8-H9-V3xofzANIYzZ1rId76-I=w1360-h768-rw-no')");
+    $("#bgimage").css("overflow-x", "hidden");
+    $("h1").css("font-weight", "600");
+  });
--- Web/js/script.js
+++ Web/js/script.js
@@ -1,8 +1,8 @@
 // <![CDATA[
 $(function() {
 	// Slider
-	$('#coin-slider').coinslider({width:960,height:328,opacity:1});
-	
+	//$('#coin-slider').coinslider({width:960,height:328,opacity:1});
+	$('#coin-slider').coinslider({width:500,height:150,opacity:1});
 	// radius Box
 	$('.wp-pagenavi a, .wp-pagenavi .current, ul.menusm li a.top_level, ul.menusm li ul, .index_rmb, .post-leav a').css({"border-radius":"5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
 	
